58e2008bf218fb4f05cda1288350f73e49b0f8bd
Andrea.DeStefano <enricoandrea.destefano@gmail.com>
2022-10-11 17:35:51 +0200

    Aggiungere i file di progetto.

---
+++ DBContext/BoolflixDbContext.cs
@@ -0,0 +1,22 @@
+﻿using csharp_boolflix.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace csharp_boolflix.DBContext
+{
+    public class BoolflixDbContext : DbContext
+    {
+        public DbSet<Film> Films { get; set; }
+        public DbSet<TVSeries> TvSeries { get; set; }
+        public DbSet<Episode> Episodes { get; set; }
+        public DbSet<MediaInfo> MediaInfos { get; set; }
+        public DbSet<Actor> Actors { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<Feature> Features { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string stringConn = "Data Source=localhost;Initial Catalog=db-boolflix;Integrated Security=True";
+
+            optionsBuilder.UseSqlServer(stringConn);
+        }
+    }
+}
